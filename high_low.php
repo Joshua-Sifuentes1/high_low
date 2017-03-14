5669d81cdd74cdcd4641d8b556a397a44ae30f17
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-14 10:13:24 -0500

    Added STDIN to each if and elseif statement, along with echoing "Good Guess", outside the loop when the while loop conditions do not apply anymore.

---
+++ high_low.php
@@ -0,0 +1,28 @@
+<?php 
+	$randomNumber = mt_rand(0, 100);
+	var_dump($randomNumber);
+	fwrite(STDOUT, "Guess a number between 0 and 100." . PHP_EOL);
+	$userGuess = fgets(STDIN);
+
+	while($userGuess != $randomNumber) {
+
+		echo "Guess? $userGuess" . PHP_EOL;
+
+		if($userGuess < $randomNumber) {
+			fwrite(STDOUT, "Higher!" . PHP_EOL);
+			fwrite(STDOUT, "Guess again." . PHP_EOL);
+			$userGuess = fgets(STDIN);
+
+		} else if($userGuess > $randomNumber) {
+			fwrite(STDOUT, "Lower!" . PHP_EOL);
+			fwrite(STDOUT, "Guess again." . PHP_EOL);
+			$userGuess = fgets(STDIN);
+
+		} else {
+			fwrite(STDOUT, "Try again" . PHP_EOL);
+		}
+	}
+
+	echo "Great Guess! You got it!" . PHP_EOL;
+
+?>
